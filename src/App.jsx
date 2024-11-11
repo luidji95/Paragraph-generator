01c69a7706eb4f593f928480d961b40eb32db9d9
Milos Petrovic <milos.petrovic09.95@gmail.com>
2024-11-12 00:02:57 +0100

    Set up paragraph rendering function structure and CSS updates

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [number, setNumber] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://example.org/post", {
+          mode: "no-cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log("Fetched data:", data);
+        // Ovde možeš postaviti data u state ako je potrebno: setState(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const renderParagraphs = (value) => {
+    console.log(value);
+  };
+
+  return (
+    <>
+      <h3 className="h3">TIRED OF BORING LOREM IPSUM?</h3>
+      <div className="generate-div">
+        <p className="parag">Paragraphs:</p>
+        <input
+          type="number"
+          className="input"
+          value={number}
+          min="1"
+          onChange={(e) => setNumber(Math.max(1, e.target.value))}
+        />
+        <button
+          className="generate-button"
+          onClick={() => renderParagraphs(number)}
+        >
+          Generate
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
